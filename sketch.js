9782804c6c7ec27cb2a5c9ec0853ff0906ff960e
Bailey Tincher <btincher99@gmail.com>
2017-12-11 13:57:48 -0600

    Works for bubble and selection

--- sketch.js
+++ sketch.js
@@ -1,75 +1,58 @@
-var dataBank = [];
-
-function setup() {
-	createCanvas(1000, 300);
-	pixelDensity(1);
-	background(200);
-	
-	var arr = [];
-	
-	for (var i = 0; i < 20; i++) {
-		arr.push(Math.random());
-	}	
-	
-	dispPoints(arr);
-	selectionSort(arr);
-	console.log(dataBank.length);
-}
-
-var i = 0;
-function keyPressed() {
-	if (keyCode === LEFT_ARROW && i < dataBank.length){
-		console.log(i);
-		clear();
-		background(200);
-		dispPoints(dataBank[i]);
-		i++;
-	}
-	return false;
-}
-
-function dispPoints(data) {
-	var ptsDisp = [];
-	fill(0);
-	var delx = (canvas.width - 20) / data.length; //Padding of width 10 on either side
-	
-	for(var i = 0; i < data.length; i++) {
-		var pnt = [10 + delx * i, canvas.height - (data[i] * canvas.height)];
-		
-		ellipse(pnt[0], pnt[1], 10, 10);
-		if(ptsDisp.length > 0) {
-			line(ptsDisp[ptsDisp.length - 1][0], ptsDisp[ptsDisp.length - 1][1], pnt[0], pnt[1]);
-		}
-		ptsDisp.push(pnt);
-	}
-}
-
 function arrCopy(arr) {
-	var tempArr = [];
-	for (var a = 0; a < arr.length; a++) {
-		tempArr.push(arr[a]);
-	}
-	return tempArr;
-}
-	
-function bubbleSort(array) {
-    var temp = 0;
-    for (var i = 0; i < array.length - 1; i++) {
-        for (var j = 0; j < array.length - 1 - i; j++) {
-            if (array[j] > array[j + 1]) {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;			
-				
-				dataBank.push(arrCopy(array));
-                }
-            }
-			
-        }
-    return array;
+  var tempArr = [];
+  for (var a = 0; a < arr.length; a++) {
+    tempArr.push(arr[a]);
+  }
+  return tempArr;
 }
 
-function selectionSort(list) {
+//Selection sort.
+var selection = function( p ) {
+  var dataBank = [];
+
+  p.setup = function() {
+    p.createCanvas(1000, 300);
+    p.pixelDensity(1);
+    p.background(200);
+  
+    var arr = [];
+  
+    for (var i = 0; i < 20; i++) {
+      arr.push(Math.random());
+    } 
+  
+    dispPoints(arr);
+    selectionSort(arr);
+  };
+
+  var i = 0;
+  p.keyPressed = function() {
+    if (p.keyCode === p.LEFT_ARROW && i < dataBank.length){
+      p.clear();
+      p.background(200);
+      dispPoints(dataBank[i]);
+      i++;
+    }
+    return false;
+  }
+
+  function dispPoints(data) {
+    var ptsDisp = [];
+    p.fill(0);
+    var delx = (p.canvas.width - 20) / data.length; //Padding of width 10 on either side
+    
+    for(var i = 0; i < data.length; i++) {
+      var pnt = [10 + delx * i, p.canvas.height - (data[i] * p.canvas.height)];
+    
+      p.ellipse(pnt[0], pnt[1], 10, 10);
+      if(ptsDisp.length > 0) {
+        p.line(ptsDisp[ptsDisp.length - 1][0], ptsDisp[ptsDisp.length - 1][1], pnt[0], pnt[1]);
+      }
+      ptsDisp.push(pnt);
+    }
+  }
+
+  function selectionSort(list) {
     var minVal = 0;
     var temp = 0;
     var index = 0;
@@ -81,18 +64,82 @@ function selectionSort(list) {
             if (list[j] < minVal) {
                 minVal = list[j];
                 index = j;
-				dataBank.push(arrCopy(list));
+                dataBank.push(arrCopy(list));
             }
         }
         if (minVal < list[i]) {
             temp = list[i];
             list[i] = list[index];
             list[index] = temp;
+            dataBank.push(arrCopy(list));
         }
-		
     }
     return list;
-}
+  }
+  
+};
+var myp5 = new p5(selection, 'ss');
 
+//Bubble sort
+var bubble = function( p ) { 
+  var dataBank = [];
 
+  p.setup = function() {
+    p.createCanvas(1000, 300);
+    p.pixelDensity(1);
+    p.background(200);
+  
+    var arr = [];
+  
+    for (var i = 0; i < 20; i++) {
+      arr.push(Math.random());
+    } 
+  
+    dispPoints(arr);
+    bubbleSort(arr);
+  };
 
+  var i = 0;
+  p.keyReleased = function() {
+    if (p.keyCode === p.LEFT_ARROW && i < dataBank.length){
+      p.clear();
+      p.background(200);
+      dispPoints(dataBank[i]);
+      i++;
+    }
+    return false;
+  }
+  function dispPoints(data) {
+    var ptsDisp = [];
+    p.fill(0);
+    var delx = (p.canvas.width - 20) / data.length; //Padding of width 10 on either side
+    
+    for(var i = 0; i < data.length; i++) {
+      var pnt = [10 + delx * i, p.canvas.height - (data[i] * p.canvas.height)];
+    
+      p.ellipse(pnt[0], pnt[1], 10, 10);
+      if(ptsDisp.length > 0) {
+        p.line(ptsDisp[ptsDisp.length - 1][0], ptsDisp[ptsDisp.length - 1][1], pnt[0], pnt[1]);
+      }
+      ptsDisp.push(pnt);
+    }
+  }
+
+  function bubbleSort(array) {
+    var temp = 0;
+    for (var i = 0; i < array.length - 1; i++) {
+        for (var j = 0; j < array.length - 1 - i; j++) {
+            if (array[j] > array[j + 1]) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;      
+        
+                dataBank.push(arrCopy(array));
+                }
+            }
+      
+        }
+    return array;
+  }
+};
+var myp5 = new p5(bubble, 'bs');
