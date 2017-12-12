0c460afa62b029aae4871721e2c9dd795c0aa266
Bailey Tincher <btincher99@gmail.com>
2017-12-11 23:47:07 -0600

    FUCKING. FINISHED.

--- sketch.js
+++ sketch.js
@@ -6,6 +6,18 @@ function arrCopy(arr) {
   return tempArr;
 }
 
+var arr1 = [];
+var arr2 = [];
+var arr3 = [];
+function setup() {
+  for (var i = 0; i < 20; i++) {
+    var n = Math.random();
+    arr1.push(n);
+    arr2.push(n);
+    arr3.push(n);
+  } 
+}
+
 //Selection sort.
 var selection = function( p ) {
   var dataBank = [];
@@ -14,20 +26,16 @@ var selection = function( p ) {
     p.createCanvas(1000, 300);
     p.pixelDensity(1);
     p.background(200);
-  
-    var arr = [];
-  
-    for (var i = 0; i < 20; i++) {
-      arr.push(Math.random());
-    } 
-  
-    dispPoints(arr);
-    selectionSort(arr);
+    
+    dispPoints(arr1);
+    selectionSort(arr1);
+    console.log("ss:" + dataBank.length);
   };
 
   var i = 0;
+  
   p.keyPressed = function() {
-    if (p.keyCode === p.LEFT_ARROW && i < dataBank.length){
+    if (keyCode === LEFT_ARROW && i < dataBank.length){
       p.clear();
       p.background(200);
       dispPoints(dataBank[i]);
@@ -36,13 +44,14 @@ var selection = function( p ) {
     return false;
   }
 
+
   function dispPoints(data) {
     var ptsDisp = [];
     p.fill(0);
     var delx = (p.canvas.width - 20) / data.length; //Padding of width 10 on either side
     
     for(var i = 0; i < data.length; i++) {
-      var pnt = [10 + delx * i, p.canvas.height - (data[i] * p.canvas.height)];
+      var pnt = [10 + delx * i, p.canvas.height - 10 - (data[i] * (p.canvas.height / 1.5))];
     
       p.ellipse(pnt[0], pnt[1], 10, 10);
       if(ptsDisp.length > 0) {
@@ -63,15 +72,14 @@ var selection = function( p ) {
         for (var j = i; j < list.length; j++) {
             if (list[j] < minVal) {
                 minVal = list[j];
-                index = j;
-                dataBank.push(arrCopy(list));
+                index = j; 
             }
+            dataBank.push(arrCopy(list));
         }
         if (minVal < list[i]) {
             temp = list[i];
             list[i] = list[index];
             list[index] = temp;
-            dataBank.push(arrCopy(list));
         }
     }
     return list;
@@ -88,15 +96,10 @@ var bubble = function( p ) {
     p.createCanvas(1000, 300);
     p.pixelDensity(1);
     p.background(200);
-  
-    var arr = [];
-  
-    for (var i = 0; i < 20; i++) {
-      arr.push(Math.random());
-    } 
-  
-    dispPoints(arr);
-    bubbleSort(arr);
+
+    dispPoints(arr2);
+    bubbleSort(arr2);
+    console.log("bs:" + dataBank.length);
   };
 
   var i = 0;
@@ -115,7 +118,7 @@ var bubble = function( p ) {
     var delx = (p.canvas.width - 20) / data.length; //Padding of width 10 on either side
     
     for(var i = 0; i < data.length; i++) {
-      var pnt = [10 + delx * i, p.canvas.height - (data[i] * p.canvas.height)];
+      var pnt = [10 + delx * i, p.canvas.height - 10 - (data[i] * (p.canvas.height / 1.5))];
     
       p.ellipse(pnt[0], pnt[1], 10, 10);
       if(ptsDisp.length > 0) {
@@ -126,20 +129,128 @@ var bubble = function( p ) {
   }
 
   function bubbleSort(array) {
-    var temp = 0;
-    for (var i = 0; i < array.length - 1; i++) {
-        for (var j = 0; j < array.length - 1 - i; j++) {
-            if (array[j] > array[j + 1]) {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;      
-        
-                dataBank.push(arrCopy(array));
-                }
+    for (var i = 1; i < array.length; i++) {
+        for (var j = 1; j < array.length; j++) {
+            if (array[j] < array[j - 1]) {
+                var temp = array[j];
+                array[j] = array[j - 1];
+                array[j - 1] = temp;
             }
-      
+            dataBank.push(arrCopy(array));
         }
+    }
     return array;
   }
 };
 var myp5 = new p5(bubble, 'bs');
+
+//Quick sort
+var quick = function( p ) { 
+  var dataBank = [];
+
+  p.setup = function() {
+    p.createCanvas(1000, 300);
+    p.pixelDensity(1);
+    p.background(200);
+  
+    dispPoints(arr3);
+    quickSort(arr3, 0, arr3.length);
+
+    console.log("qs:" + dataBank.length);
+  };
+
+  var i = 0;
+  p.keyReleased = function() {
+    if (p.keyCode === p.LEFT_ARROW && i < dataBank.length){
+      p.clear();
+      p.background(200);
+      dispPoints(dataBank[i]);
+      i++;
+    }
+    return false;
+  }
+  function dispPoints(data) {
+    var ptsDisp = [];
+    p.fill(0);
+    var delx = (p.canvas.width - 20) / data.length; //Padding of width 10 on either side
+    
+    for(var i = 0; i < data.length; i++) {
+      var pnt = [10 + delx * i, p.canvas.height - 10 - (data[i] * (p.canvas.height / 1.5))];
+    
+      p.ellipse(pnt[0], pnt[1], 10, 10);
+      if(ptsDisp.length > 0) {
+        p.line(ptsDisp[ptsDisp.length - 1][0], ptsDisp[ptsDisp.length - 1][1], pnt[0], pnt[1]);
+      }
+      ptsDisp.push(pnt);
+    }
+  }
+
+  function quickSort(arr, lo, hi) {
+    if (hi - lo < 2) {
+      if (hi - lo == 1) {
+        if(arr[lo] > arr[hi]) {
+          swap(arr, lo, hi);
+          dataBank.push(arrCopy(arr));
+        }
+      }
+      return;
+    }
+    partition(arr, lo, hi);
+  }
+
+  function getPivotInd(arr, lo , hi) {
+    var a = arr[lo + 1];
+    var b = arr[(lo + hi) / 2];
+    var c = arr[hi];
+
+    if ((a - b) * (c - a) >= 0) { // a >= b and a <= c OR a <= b and a >= c
+      return lo + 1;
+    } else if ((b - a) * (c - b) >= 0) { // b >= a and b <= c OR b <= a and b >= c
+      return (lo + hi) / 2;
+    } else {
+      return hi;
+    }
+  }
+
+  function partition(arr, loInit, hiInit) {
+    var lo = loInit;
+    var hi = hiInit;
+    var pivInd = getPivotInd(arr, lo, hi);
+
+    swap(arr, lo, pivInd);
+    pivInd = lo;
+    lo++;
+
+    for (var a = lo; a <= hi && lo != hi; a++) {
+      if (arr[a] < arr[pivInd]) {
+        swap(arr, a, lo);
+        lo++;
+      } else {
+        swap(arr, a, hi);
+        hi--;
+        a--; //Since we need to re-check the value at a
+      }
+      dataBank.push(arrCopy(arr));
+    }
+
+    if (arr[pivInd] > arr[lo]) {
+      swap(arr, pivInd, lo);
+      pivInd = lo;
+    } else {
+      swap(arr, pivInd, lo - 1);
+      pivInd = lo - 1;
+    }
+
+    dataBank.push(arrCopy(arr));
+
+    quickSort(arr, loInit, pivInd - 1);
+    quickSort(arr, pivInd + 1, hiInit);
+  }
+
+  function swap(arr, ind1, ind2) {
+    var temp = arr[ind1];
+    arr[ind1] = arr[ind2];
+    arr[ind2] = temp;
+  }
+};
+var myp5 = new p5(quick, 'qs');
